043e7daa11985c7582a3d754b9d79f85a0da220b
utkarsh barsaiyan <utkarsh060903@gmail.com>
2025-01-14 23:54:41 +0530

    navbar responsive

--- frontend/src/Components/Navbar/extra.jsx
+++ frontend/src/Components/Navbar/extra.jsx
@@ -1,86 +1,344 @@
-import styled from "styled-components";
-import {NavLink as Link} from "react-router-dom";
-import {FaBars} from "react-icons/fa";
 
-export const Nav = styled.nav`
-    background: #000;
-    height: 80px;
-    width: auto;
-    display: flex;
-    justify-content: space-between;
-    // padding: auto;
-    padding: 0.5rem calc((100vw - 1000px)/2);
-    z-index: 10;
-`
-
-export const NavLink = styled(Link)`
-    color: #fff;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-
-    &.active{
-        color: #15cdfc;
-    }
-`
-
-export const Bars = styled(FaBars)`
-    display: none;
-    color: #fff;
-
-    @media screen and (max-width: 768px){
-        display: block;
-        position: absolute;
-        top: 0;
-        right: 0;
-        transform: translate(-100% , 75%);
-        font-size: 1.8rem;
-        cursor: pointer;
-    }
-`
-
-export const NavMenu = styled.div`
-    display: flex;
-    align-items: center;
-    margin-right: -24px;
-    
-    @media screen and (max-width: 768px)
-    {
-        display: none;
-    }
-`
-
-export const NavBtn = styled.nav`
-    display: flex;
-    align-items: center;
-    margin-right: -45px;
-    @media screen and (max-width: 768px)
-    {
-        display: none;
-    }
-`
-
-export const NavBtnLink = styled(Link)`
-    border-radius : 4px;
-    background: #256ce1;
-    padding: 10px 22px;
-    // margin-right: -100px;
-    color: #fff;
-    border: none;
-    outline: none;
-    // display: flex;
-    cursor: pointer;
-    transition: all 0.2 ease-in-out;
-    text-decoration: none;
-
-    &:hover{
-        transition: all 0.2 ease-in-out;
-        background: #fff;
-        color: #010606;
-    }
-`
+import "../../App.css";
+import image22 from "../../images/logo2_145x80.png";
+import "./extra.css";
+import { Nav, Bars, NavLink, NavBtn, NavBtnLink, NavMenu } from './NavbarElements';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import { useContext, useState } from 'react';
+import HamBurger from "../HamBurger/Hamburger";
 
 
+const Navbar = () => {
+  const {user , setUser} = useContext(UserContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    setUser(null);
+    navigate("/");
+  };
+ 
+ 
+
+  return (
+    <>
+      <Nav>
+        {/* ------lnmiit main logo-------- */}
+
+        <NavLink to="/">
+          <img src={image22} alt="logo" className="imge"
+          onClick={(e) => {
+            e.preventDefault(); 
+            window.location.href = "/"; //when image is clicked, the website refreshes and takes us to home page
+          }} />
+        </NavLink>
+
+         {/* ------------------- hamburger menu --------------------------- */}
+
+        <HamBurger/>
+          
+        
+
+        <div className="main-div">
+        <NavMenu>
+
+              {/* ----------------- HOME ------------------- */}  
+
+          <NavLink
+            to="/home"
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'inherit',
+            })}
+            
+            onClick={(e) => {
+              e.preventDefault(); 
+              const navbarHeight = document.querySelector(Nav)?.offsetHeight || 0; //finds navbar fixed height
+              const element = document.getElementById("home");
+              if (element) {
+                const elementPosition = element.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.scrollY  - navbarHeight;
+                window.scrollTo({
+                  top: offsetPosition,
+                  behavior: "smooth"
+                });
+              }
+            }}
+          >
+            Home
+          </NavLink>
+              {/* --------------------ABOUT US ----------------------- */}
+
+          <NavLink
+            to="/about"
+            className={"abt-btn"}
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'inhert',
+            })}
+
+            onClick={(e) => {
+              e.preventDefault(); 
+              const navbarHeight = document.querySelector(Nav)?.offsetHeight || 0; //finds navbar fixed height
+              const element = document.getElementById("outer");
+              if (element) {
+                const elementPosition = element.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.scrollY  - navbarHeight;
+                window.scrollTo({
+                  top: offsetPosition,
+                  behavior: "smooth"
+                });
+              }
+            }}
+          >
+            About Us
+          </NavLink>
+
+          {/* ----------------- SERVICES ------------------- */}
+
+          <NavLink
+            to="/services"
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'inherit',
+            })}
+
+            onClick={(e) => {
+              e.preventDefault(); 
+              const navbarHeight = document.querySelector(Nav)?.offsetHeight || 0; //finds navbar fixed height
+              const element = document.getElementById("services");
+              if (element) {
+                const elementPosition = element.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.scrollY  - navbarHeight;
+                window.scrollTo({
+                  top: offsetPosition,
+                  behavior: "smooth"
+                });
+              }
+            }}
+          >
+            Services
+          </NavLink>
+
+          {/* ----------------- GALLERY ------------------- */}
+
+          <NavLink
+            to="/gallery"
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'inherit',
+            })}
+
+            onClick={(e) => {
+              e.preventDefault(); 
+              const navbarHeight = document.querySelector(Nav)?.offsetHeight || 0; //finds navbar fixed height
+              const element = document.getElementById("gallery");
+              if (element) {
+                const elementPosition = element.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.scrollY  - navbarHeight;
+                window.scrollTo({
+                  top: offsetPosition,
+                  behavior: "smooth"
+                });
+              }
+            }}
+          >
+            Gallery
+          </NavLink>
+
+          {/* -------------- AVAILABILITY ------------------------ */}
+
+          <NavLink
+            to="/availability"
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'inherit',
+            })}
+            
+            onClick={(e) => {
+              e.preventDefault(); 
+              const navbarHeight = document.querySelector(Nav)?.offsetHeight || 0; //finds navbar fixed height
+              const element = document.getElementById("availability");
+              if (element) {
+                const elementPosition = element.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.scrollY  - navbarHeight;
+                window.scrollTo({
+                  top: offsetPosition,
+                  behavior: "smooth"
+                });
+              }
+            }}
+          >
+            Availability
+          </NavLink>
+
+          {/* ------------------- GUIDELINES ---------------------- */}
+
+          <NavLink
+            to="/guidelines"
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'inherit',
+            })}
+
+            onClick={(e) => {
+              e.preventDefault(); 
+              //code to take us at starting pt of guidelines div when scrolled
+              const navbarHeight = document.querySelector(Nav)?.offsetHeight || 0; //finds navbar fixed height
+              const element = document.getElementById("guidelines");
+              if (element) {
+                const elementPosition = element.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.scrollY  - navbarHeight;
+                window.scrollTo({
+                  top: offsetPosition,
+                  behavior: "smooth"
+                });
+              }
+            }}
+          >
+            Guidelines
+          </NavLink>
+
+          {/* -------------------- CONTACT US ------------------------- */}
+
+          <NavLink
+            to="/contactus"
+            style={({ isActive }) => ({
+              color: isActive ? 'red' : 'inherit',
+            })}
+
+            onClick={(e) => {
+              e.preventDefault(); 
+              const navbarHeight = document.querySelector(Nav)?.offsetHeight || 0; //finds navbar fixed height
+              const element = document.getElementById("contact");
+              if (element) {
+                const elementPosition = element.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.scrollY  - navbarHeight;
+                window.scrollTo({
+                  top: offsetPosition,
+                  behavior: "smooth"
+                });
+              }
+            }}
+          >
+            Contact Us
+          </NavLink>
+
+        </NavMenu>
+        </div>
+
+        {/* --------button part of navbar--------------- */}
+
+        <NavBtn className="btn-div">
+          {/* <NavBtnLink className="btn1" to="/form">
+            BOOK NOW
+          </NavBtnLink> */}
+         
+
+          {user && <NavBtnLink onClick={handleLogout}>{user.username}</NavBtnLink>}
+
+         {!user && (
+           <NavBtnLink className="btn2" to="/LoginModal"> 
+            BOOK NOW
+          </NavBtnLink>
+         )}
+        </NavBtn>
+
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // import React from 'react'
+// // import "../../App.css"
+// // import image22 from "../../images/logo2_145x80.png"
+
+
+// // // import { NavLink } from 'react-router-dom';
+// // import { Nav, Bars, NavLink,  NavBtn, NavBtnLink, NavMenu } from './NavbarElements';
+
+
+
+
+// // const Navbar = () => { 
+// //   return(
+// //     <> 
+// //       <Nav>
+// //         <NavLink to='/'>
+// //           <img src={image22} alt="logo" className="imge" />
+// //         </NavLink>
+// //         <Bars/>
+// //         <NavMenu>
+// //           <NavLink to="/about" activeStyle>
+// //             About Us
+// //           </NavLink>
+// //           <NavLink to="/services" activeStyle>
+// //             Services
+// //           </NavLink>
+// //           <NavLink to="/charges" activeStyle>
+// //             Charges
+// //           </NavLink>
+// //           <NavLink to="/availability" activeStyle>
+// //             Availability
+// //           </NavLink>
+// //           <NavLink to="/guidelines" activeStyle>
+// //             Guidelines
+// //           </NavLink>
+// //           {/* <NavLink to="/gallery" activeStyle>
+// //             Gallery
+// //           </NavLink> */}
+// //           <NavLink to="/contactus" activeStyle>
+// //             Contact Us
+// //           </NavLink>
+// //           {/* <NavLink  to="/sign-up" activeStyle>
+// //             Sign Up
+// //           </NavLink> */}
+// //         </NavMenu>
+// //         <NavBtn>
+// //           <NavBtnLink className={"btn1"} to="/LoginModal">
+// //             SIGN IN
+// //           </NavBtnLink>
+// //           <NavBtnLink className={"btn2"} to="/form">
+// //             BOOK NOW
+// //           </NavBtnLink>
+// //         </NavBtn>
+// //       </Nav>    
+// //     </>
+// //   );
+  
+// // };
+
+
+// // export default  Navbar;
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,198 +1,83 @@
 // import React from "react";
-import Card from "../../Components/Cards/Card.jsx"
-import "../../Components/Cards/Card.css"
-import Footer from "../../Components/Footer/Footer.jsx"
+import Card from "../../Components/Cards/Card.jsx";
+import "../../Components/Cards/Card.css";
+import Footer from "../../Components/Footer/Footer.jsx";
 // import Footer from "../components/Footer/Footer.js";
 import About from "../About/about.jsx";
 import "../About/about.css";
-import Guidelines from "../Guidelines/guidelines.jsx"
+import Guidelines from "../Guidelines/guidelines.jsx";
 // import Guidelines from "./guidelines"
 import Availability from "../Availability/Availability.jsx";
 // import Availability from "./availabilty"
-// import ContactUS from 
+// import ContactUS from
 import Contactus from "../ContactUs/contactus.jsx";
-import download33 from '../../images/download33.png';
-import lnmiit from "../../images/lnmiit.jpg"
-import lnmiit1 from "../../images/lnmiit1.jpg"
+import download33 from "../../images/download33.png";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <div
-        id="home"
-        style={{
-          top: "80px",
-          position: "relative",
-          overflowX: "hidden", // Hide any horizontal overflow
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            textAlign: "center",
-            position: "relative",
-            width: "100%",
-          }}
-        >
-          <img
-            src={download33}
-            className="lnm"
-            alt="LNMIIT Jaipur"
-            style={{
-              width: "100%",
-              maxWidth: "100vw", // Prevents the image from exceeding the viewport width
-              height: "100vh",
-              objectFit: "cover",
-            }}
-          />
-          <div
-            style={{
-              position: "absolute",
-              top: "21.5%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: "100%",
-              maxWidth: "100vw", 
-              padding: "0 20px",
-              boxSizing: "border-box",
-            }}
-          >
+      <div id="home">
+        <div id="img-outer-div">
+          <img src={download33} className="lnm" alt="LNMIIT Jaipur" />
+          <div className="main-heading-div">
             <h1 className="heading1">
-              Welcome to LNMIIT Jaipur Guest House Booking Website
+              Welcome to LNMIIT Jaipur Guest House Booking System
             </h1>
             <h2 className="h2-heading">We are not away from home</h2>
-            <div className="start-cards" style={{ display: "flex", justifyContent: "center", gap:"20px", paddingLeft: "200px" }}>
+            <div className="start-cards">
               <div className="cardss">
                 <Card
                   title={"STANDARD ROOMS IN GUEST HOUSE"}
                   info={
-                    "12 Standard Rooms - Ground Floor - 8 Rooms, First Floor - 4 Rooms."
+                    <ul>
+                      <li>12 Standard Rooms</li>
+                      <li>Ground Floor - 8 Rooms</li>
+                      <li>First Floor - 4 Rooms</li>
+                    </ul>
                   }
                 />
               </div>
+
               <div className="cardss">
                 <Card
                   title={"DELUXE ROOMS IN GUEST HOUSE"}
-                  info={"2 VIP Rooms - Ground Floor - 0 Rooms, First Floor - 2 Rooms."}
+                  info={
+                    <ul>
+                      <li>3 VIP Rooms</li>
+                      <li>Ground Floor - 1 Room</li>
+                      <li>First Floor - 2 Rooms</li>
+                    </ul>
+                  }
                 />
               </div>
             </div>
           </div>
         </div>
+
         <div>
           <About />
         </div>
+
         <div>
           <Availability />
         </div>
+
         <div>
           <Guidelines />
         </div>
+
         <div>
           <Contactus />
         </div>
+        
         <div>
           <Footer />
         </div>
+
       </div>
     </>
   );
 };
 
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // import React from 'react'
-// // import './Home.css'
-// // import Navbar from '../../Components/Navbar/Navbar'
-// // import Header from '../Header/Header'
-
-// // const Home = () => {
-// //   return (
-// //     <div className='home'>
-// //         <Header />
-// //     </div>
-// //   )
-// // }
-
-// // export default Home
-
-
-
-
-// import React, { useEffect, useRef } from "react";
-// import "./Home.css";
-// import gsap from "gsap";
-// import { useGSAP } from "@gsap/react";
-// import { ScrollTrigger } from "gsap/all";
-// import Header from "../Header/Header";
-// import Availability from "../Availability/Availability";
-// import AboutUs from "../AboutUs/AboutUs";
-// gsap.registerPlugin(ScrollTrigger);
-
-// const Home = () => {
-
-//   const boxref = useRef();
-  
-//   useEffect(() => {
-//     gsap.to(".navbar", {
-//       backgroundColor: "black",
-//       duration: 0.5,
-//       height: "80px",
-//       scrollTrigger: {
-//         trigger: ".navbar",
-//         scroller: "body",
-//         start: "top -10%",
-//         end: "top -11%",
-//         scrub: 1,
-//       },
-//     });
-// } , [])
-
-//   return (
-//     <div className="home" id="home">
-//       <Header />
-//       <div id="availability">
-//         <Availability />
-//       </div>
-//       <div>
-//         <AboutUs />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
